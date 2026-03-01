25c798d63e4cf985bf30c05762074b8206dd631b
SkyeAv <skye.a.goetz@gmail.com>
2026-03-01 15:56:28 -0800

    Improved API Output

--- lib/dbssert_api/api.py
+++ lib/dbssert_api/api.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from typing import Any
 from os import environ as env
 import uvicorn
-import json
 import duckdb
 import polars as pl
 
@@ -10,22 +9,42 @@ APP: Any = FastAPI()
 DBSSERT_PATH: str = env["DBSSERT_PATH"]
 
 @APP.get("/curies-with-ner")
-def get_curies_with_ner(entity: str) -> str:
+def get_curies_with_ner(entity: str) -> list[dict[str, Any]]:
   p: str = DBSSERT_PATH
   with duckdb.connect(p, read_only=True) as conn:
     entity = entity.lower()
-    sql: str = f"SELECT * FROM SYNONYMS S JOIN CURIES C ON S.CURIE_ID = C.CURIE_ID WHERE S.SYNONYM = '{entity}' LIMIT 10;"
+    sql: str = f"""
+    SELECT 
+      C.CURIE,
+      C.PREFERRED_NAME,
+      S.SYNONYM,
+      G.CATEGORY_NAME,
+      C.TAXON_ID
+    FROM SYNONYMS S
+    JOIN CURIES C ON S.CURIE_ID = C.CURIE_ID 
+    JOIN CATEGORIES G ON C.CATEGORY_ID = G.CATEGORY_ID
+    WHERE S.SYNONYM = '{entity}' LIMIT 20;
+    """
     df: pl.DataFrame = conn.sql(sql).pl()
-    return json.dumps(df.to_dicts())
+    return df.to_dicts()
 
 @APP.get("/cannoical-curie-information")
-def get_cannoical_curie_information(curie: str) -> str:
+def get_cannoical_curie_information(curie: str) -> list[dict[str, Any]]:
   p: str = DBSSERT_PATH
   with duckdb.connect(p, read_only=True) as conn:
-    sql: str = f"SELECT * FROM CURIES C WHERE C.CURIE = '{curie}' LIMIT 1;"
+    sql: str = f"""
+    SELECT
+      C.CURIE,
+      C.PREFERRED_NAME,
+      G.CATEGORY_NAME,
+      C.TAXON_ID
+    FROM CURIES C 
+    JOIN CATEGORIES G ON C.CATEGORY_ID = G.CATEGORY_ID
+    WHERE C.CURIE = '{curie}' LIMIT 10;
+    """
     df: pl.DataFrame = conn.sql(sql).pl()
-    return json.dumps(df.to_dicts())
+    return df.to_dicts()
 
-def run_api() -> None:
+def serve_api() -> None:
   app: Any = APP
-  uvicorn.run(app, host="0.0.0.0", port=8052, reload=True)
+  uvicorn.run(app, host="0.0.0.0", port=8052)
